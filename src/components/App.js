7bdd41a0e53c9c1bf8df1683187fedcdb0cdd25f
Marilia <marilia.correa.melo@gmail.com>
2023-05-02 23:46:58 -0300

    Template adicionado e componentes criados

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import react from "react"
+import Body from "./Body"
+import NavBar from "./NavBar"
+import Posts from "./Posts"
+import SideBar from "./SideBar"
+import Stories from "./Stories"
+import Suggestions from "./Suggestions"
+import User from "./User"
+
+export default function App(){
+    return()
+}
