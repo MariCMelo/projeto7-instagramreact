f3e2218cb694a23b7ad5be08d4c3ce917352c184
Marilia <marilia.correa.melo@gmail.com>
2023-05-06 00:00:24 -0300

    Dados dinâmicos feitos mas faltando alguns detalhes

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,65 +1,101 @@
-export default function Posts(){
-    const posts = [
-        {account:"meowed",imgAvatar:"assets/img/meowed.svg", altAvatar:"meowed", imgPost:"assets/img/gato-telefone.svg", altPost:"gato-telefone", imgLike:"assets/img/respondeai.svg", altLike:"respondeai", accountLike:"respondeai", likeNumber:"1583"},
-        {account:"barked",imgAvatar:"assets/img/barked.svg", altAvatar:"barked", imgPost:"assets/img/dog.svg", altPost:"dog", imgLike:"assets/img/adorable_animals.svg", altLike:"adorable_animals", accountLike:"adorable_animals",likeNumber:"523"},
-    ]
-    
-    return(
-        <div  className="posts">
-            {posts.map((post) => (
-                <Post
-                    key={post.account}
-                    avatarImage={post.imgAvatar}
-                    avatarAlt={post.altAvatar}
-                    postImage={post.imgPost}
-                    postAlt={post.altPost}
-                    likeImage={post.imgLike}
-                    likeAlt={post.altLike}
-                    likeNumber={post.likeNumber}
-                    />
-                    ))}   
-        </div>
-    );
+import { useState } from "react";
+
+export default function Posts() {
+  const posts = [
+    { account: "meowed", imgAvatar: "assets/img/meowed.svg", altAvatar: "meowed", imgPost: "assets/img/gato-telefone.svg", altPost: "gato-telefone", imgLike: "assets/img/respondeai.svg", altLike: "respondeai", accountLike: "respondeai", likeNumber: 1583 },
+    { account: "barked", imgAvatar: "assets/img/barked.svg", altAvatar: "barked", imgPost: "assets/img/dog.svg", altPost: "dog", imgLike: "assets/img/adorable_animals.svg", altLike: "adorable_animals", accountLike: "adorable_animals", likeNumber: 523 },
+  ]
+
+
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post
+          key={post.account}
+          avatarImage={post.imgAvatar}
+          avatarAlt={post.altAvatar}
+          postImage={post.imgPost}
+          postAlt={post.altPost}
+          likeImage={post.imgLike}
+          likeAlt={post.altLike}
+          likeNumber={post.likeNumber}
+        />
+      ))}
+    </div>
+  );
 }
-    function Post(props){
-        return(
-            <div data-test="post" className="post">
-            <div className="topo">
-              <div className="usuario">
-                <img data-test="post-image" src={props.avatarImage} alt={props.avatarAlt}/>
-                {props.account}
-            </div>
-              <div className="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
+function Post(props) {
+  const [saved, setSaved] = useState("bookmark-outline")
+  const [liked, setLiked] = useState("heart-outline")
+  const [likeNumber, setLikeNumber] = useState(props.likeNumber)
 
-            <div className="conteudo">
-              <img src={props.postImage} alt={props.postAlt}/>
-            </div>
-
-            <div className="fundo">
-              <div className="acoes">
-                <div>
-                  <ion-icon data-test="like-post" name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon data-test="save-post" name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div className="curtidas">
-                <img src={props.likeImage} alt={props.likeAlt}/>
-                <div className="texto">
-                  Curtido por <strong>{props.accountLike}</strong> e <strong data-test="likes-number">outras {props.likeNumber} pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        )
+  function savePost() {
+    if (saved === "bookmark-outline") {
+      setSaved("bookmark")
+    } else {
+      setSaved("bookmark-outline")
     }
+  }
 
-   
+  function likePost() {
+    if (liked === "heart-outline") {
+      setLiked("heart")
+      setLikeNumber(likeNumber + 1)
+    } else {
+      setLiked("heart-outline")
+      setLikeNumber(likeNumber - 1)
+    }
+  }
+
+  function likePostFromImage() {
+    if (liked === "heart-outline") {
+      setLiked("heart")
+      setLikeNumber(likeNumber + 1)
+    }
+  }
+
+  function addLikeNumber(){
+    
+
+  }
+
+  return (
+    <div onClick={likePostFromImage} data-test="post" className="post">
+      <div className="topo">
+        <div className="usuario">
+          <img data-test="post-image" src={props.avatarImage} alt={props.avatarAlt} />
+          {props.account}
+        </div>
+        <div className="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div className="conteudo">
+        <img src={props.postImage} alt={props.postAlt} />
+      </div>
+
+      <div className="fundo">
+        <div className="acoes">
+          <div>
+            <ion-icon onClick={likePost} data-test="like-post" name={liked}></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+            <ion-icon onClick={savePost} data-test="save-post" name={saved}></ion-icon>
+          </div>
+        </div>
+
+        <div className="curtidas">
+          <img src={props.likeImage} alt={props.likeAlt} />
+          <div className="texto">
+            Curtido por <strong>{props.accountLike}</strong> e <strong data-test="likes-number">outras {likeNumber} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,31 @@
-export default function User(){
-    return(
-        <div className="usuario">
-          <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
-          <div className="texto">
-            <span>
-              <strong data-test="name">catanacomics</strong>
-              <ion-icon data-test="edit-name" name="pencil"></ion-icon>
-            </span>
-          </div>
-        </div>
-    )
+import { useState } from "react"
+
+export default function User() {
+  const [userName, setUserName] = useState("catanacomics")
+  const [userImage, setUserImage] = useState("assets/img/catanacomics.svg")
+
+  function askName() {
+    const name = prompt("Insira seu usuário")
+    if (name !== null && name !== "" && name !== undefined) {
+      setUserName(name)
+    }
+  }
+  function askImage() {
+    const image = prompt("Insira sua imagem de perfil")
+    if (image !== null && image !== "" && image !== undefined) {
+      setUserImage(image)
+    }
+  }
+
+  return (
+    <div className="usuario">
+      <img onClick={askImage} data-test="profile-image" src={userImage} alt="imagem de perfil" />
+      <div className="texto">
+        <span>
+          <strong data-test="name">{userName}</strong>
+          <ion-icon onClick={askName} data-test="edit-name" name="pencil"></ion-icon>
+        </span>
+      </div>
+    </div>
+  )
 }
